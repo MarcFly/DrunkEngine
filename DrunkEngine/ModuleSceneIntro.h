f86df6c2b5d546b763317d4ba874d6f212a3a026
Marc Torres Jimenez <mtorres.form@gmail.com>
2018-09-13 17:14:31 +0200

    Names Changed
    Have changed the name of 5 files :)

---
+++ DrunkEngine/ModuleSceneIntro.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Module.h"
+#include "p2DynArray.h"
+#include "Globals.h"
+#include "Primitive.h"
+
+#define MAX_SNAKE 2
+
+struct PhysBody3D;
+struct PhysMotor3D;
+
+struct Block {
+	PhysBody3D* body;
+	Primitive* shape;
+
+	~Block()
+	{
+		
+	}
+};
+
+struct Sensor {
+	Cube* shape;
+	PhysBody3D* body;
+	int id;
+};
+
+class ModuleSceneIntro : public Module
+{
+public:
+	ModuleSceneIntro(Application* app, bool start_enabled = true);
+	~ModuleSceneIntro();
+
+	bool Start();
+	update_status Update(float dt);
+	bool CleanUp();
+
+	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
+
+public:
+
+};
---
+++ DrunkEngine/ModuleUI.cpp
@@ -0,0 +1,38 @@
+#include "Globals.h"
+#include "ModuleUI.h"
+#include "Application.h"
+
+
+ModuleUI::ModuleUI(Application* app, bool start_enabled) : Module(app, start_enabled)
+{
+}
+
+ModuleUI::~ModuleUI()
+{
+}
+
+bool ModuleUI::Init()
+{
+	LOG("Initiating IMGUI Module")
+	bool ret = true;
+
+	return ret;
+}
+
+update_status ModuleUI::PreUpdate(float dt)
+{
+	return UPDATE_CONTINUE;
+}
+
+update_status ModuleUI::PostUpdate(float dt)
+{
+	return UPDATE_CONTINUE;
+}
+
+bool ModuleUI::CleanUp()
+{
+	LOG("Cleaning UP IMGUI Module")
+	bool ret = true;
+
+	return ret;
+}
