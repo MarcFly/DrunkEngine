1a375a26b59d2655aff8636d0b2e7b057e53067a
Andreu997 <andreu12697@gmail.com>
2018-09-27 09:16:21 +0200

    OpenGl configuration working
    Now is possible to change the OpenGL properties through the checkboxes on the OpenGL configuration Menu.

--- DrunkEngine/OpenGLConfigWindow.cpp
+++ DrunkEngine/OpenGLConfigWindow.cpp
@@ -1,4 +1,5 @@
 #include "OpenGLConfigWindow.h"
+#include "SDL\include\SDL_opengl.h"
 
 OpenGLConfigWindow::OpenGLConfigWindow(Application * app) : Window("OpenGL Configuration", SDL_SCANCODE_UNKNOWN)
 {
@@ -23,32 +24,47 @@ void OpenGLConfigWindow::Draw()
 	{
 		if (ImGui::Checkbox("GL_Depth_Test", &gl_depth_test))
 		{
-			
+			if (gl_depth_test)
+				glEnable(GL_DEPTH_TEST);
+			else
+				glDisable(GL_DEPTH_TEST);
 		}
 		ImGui::SameLine();
 
 		if (ImGui::Checkbox("GL_Cull_Face", &gl_cull_face))
 		{
-
+			if (gl_cull_face)
+				glEnable(GL_CULL_FACE);
+			else
+				glDisable(GL_CULL_FACE);
 		}
 		ImGui::SameLine();
 
 		if (ImGui::Checkbox("GL_Lighting", &gl_lighting))
 		{
-
+			if (gl_lighting)
+				glEnable(GL_LIGHTING);
+			else
+				glDisable(GL_LIGHTING);
 		}
 
 		ImGui::Separator();
 
 		if (ImGui::Checkbox("GL_Color_Material", &gl_color_material))
 		{
-
+			if (gl_color_material)
+				glEnable(GL_COLOR_MATERIAL);
+			else
+				glDisable(GL_COLOR_MATERIAL);
 		}
 		ImGui::SameLine();
 
 		if (ImGui::Checkbox("GL_Texture_2D", &gl_texture_2d))
 		{
-
+			if (gl_texture_2d)
+				glEnable(GL_TEXTURE_2D);
+			else
+				glDisable(GL_TEXTURE_2D);
 		}
 		ImGui::SameLine();
 
